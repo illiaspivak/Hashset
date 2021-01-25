8277d8f5e7cd7a69814d8bfd0a915990a9abb3e3
illiaspivak <illia.spivak@kosickaakademia.sk>
2021-01-25 12:28:10 +0100

    Json array

--- src/sk/kosickaakademia/illiaspivak/json/Main.java
+++ src/sk/kosickaakademia/illiaspivak/json/Main.java
@@ -7,10 +7,13 @@ public class Main {
         String xml_text = "<player>\n" +
                 "<name>Milian</name>\n" +
                 "<age>32</age>\n" +
-                "<sport>hochey</sport>\n" +
+                "<sport type=\"array\">\n" +
+                "    <value>hockey</value>\n" +
+                "    <value>football</value>\n" +
+                "    <value>golf</value></sport>\n" +
                 "<active>true</active>\n" +
                 "</player>";
-        String jsonPrettyPrintString  = XML.toJSONObject(xml_text).toString();
-        System.out.println(jsonPrettyPrintString);
+        String jsonText  = XML.toJSONObject(xml_text).toString();
+        System.out.println(jsonText);
     }
 }
